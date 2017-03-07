fc9153ecdaf493c6c3816e3592ff324cba82273b
Shironee <adik_na_secret@yahoo.com>
2017-03-07 20:39:47 +0800

    Update and style the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page</p>
+			<h1 className="text-center">About</h1>
+			<p>React Weather Application</p>
+			<p>Tools used to this application:</p>
+			<ul>
+				<li><a href="https://facebook.github.io/react">React</a> - Javascript framework used.</li>
+				<li><a href="http://openweathermap.org">Open Weather Map</a> - Open Weather Map API.</li>
+			</ul>
 		</div>
 		
 	)
